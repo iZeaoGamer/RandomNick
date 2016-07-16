6200497708d9827e931ab49e53e8f564e74b57eb
Erkam Kahraman <erkamkahriman@gmail.com>
2016-07-16 10:32:06 +0200

    Create Main.php

---
+++ src/Random/Main.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Random;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as C;
+
+class Main extends implements Listener {
