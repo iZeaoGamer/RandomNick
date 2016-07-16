fa34e5dd2358fab89f9198ee07b3b21d3c76d5bf
Erkam Kahraman <erkamkahriman@gmail.com>
2016-07-16 10:35:20 +0200

    Update Main.php

--- src/Random/Main.php
+++ src/Random/Main.php
@@ -7,9 +7,9 @@ use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat as C;
 
-class Main extends implements Listener {
+class Main extends PluginBase implements Listener {
   
   public function onEnable(){
     $this->getLogger()->info(C::GREEN."Activated!");
-    
+    $this->getServer()->getPluginManager()->registerEvents($this,$this);
   }
