023d1a8046888e55bbb8c60adc42f0ebdcc08c62
Erkam Kahraman <erkamkahriman@gmail.com>
2016-07-30 12:09:05 +0200

    Update Main.php

--- src/Random/Main.php
+++ src/Random/Main.php
@@ -14,16 +14,18 @@ use pocketmine\utils\TextFormat as C;
 
 class Main extends PluginBase implements Listener {
   
+  public $prefix = TextFormat::GRAY."[".TextFormat::GOLD."Nick".TextFormat::GRAY."]".TextFormat::WHITE." ";
+  
   public function onEnable(){
-    $this->getLogger()->info(C::GREEN."Activated!");
+    $this->getLogger()->info($this->prefix.C::GREEN."Activated!");
     $this->getServer()->getPluginManager()->registerEvents($this,$this);
   }
   public function onDisable(){
-    $this->getLogger()->info(C::RED."Deactivated!");
+    $this->getLogger()->info($this->prefix.C::RED."Deactivated!");
   }
   
   public function onCommand(CommandSender $s, Command $cmd, array $args){
     switch(strtolower($cmd)){
       case "nick":
         if(count($args) != 1){
-          $sender->sendMessage(C::GOLD."Usage : /nick random");
+          $sender->sendMessage($this->prefix.C::GOLD."Benutze : /nick random");
