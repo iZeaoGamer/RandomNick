4bb7074f41b80aee6eb5c4d1af794b2bca3a7045
minTodroid <tobischu2000@gmail.com>
2016-08-01 18:42:26 +0200

    Update Main.php

--- src/Random/Main.php
+++ src/Random/Main.php
@@ -13,6 +13,8 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase {
   
+  public $this->nicks = ["LALA", "HUHU", "HAHA"];
+  
   public function onEnable(){
     $this->getLogger()->info(C::GREEN."Activated!");
   }
