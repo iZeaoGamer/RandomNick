717bdcee2113622b2b07f3f7c0a57c5b0b23c6c3
iZeaoGamer <jameshayward14@icloud.com>
2017-12-24 09:55:43 +0000

    Update Main.php

--- src/Random/Main.php
+++ src/Random/Main.php
@@ -55,7 +55,7 @@ class Main extends PluginBase {
         }
         elseif(strtolower($args[0]) === "off"){
         	$this->action_nick_off($sender);
-        }
+        return true;
         break;
     }
   }
